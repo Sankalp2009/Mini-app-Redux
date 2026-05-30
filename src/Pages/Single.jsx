2695c99b9a5882df3b05602a109cda9f1eade0bc
sankalp patel <sankalppatel38@gmail.com>
2026-05-30 19:56:15 +0530

    Enhance Single product page with improved loading state and styling; add error handling for product fetch and update CSS for better layout.

--- src/Pages/Single.jsx
+++ src/Pages/Single.jsx
@@ -1,40 +1,37 @@
-import React, {useState, useEffect} from 'react'
-import {useParams} from 'react-router'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
 function Single() {
-  const [single, setSingle] = useState({});
-  const {id} = useParams();
-  console.log(id);
- 
-  useEffect(()=>{
-    const Fetch = async()=>{
+  const [single, setSingle] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams()
+  console.log(id)
+  const { thumbnail, title, price } = single || {}
+  useEffect(() => {
+    const Fetch = async () => {
       try {
         let res = await fetch(`https://dummyjson.com/products/${id}`)
         let data = await res.json()
-        console.log(data);
+        if (!data.id) {
+          throw new Error('Product not found')
+        }
         setSingle(data)
       } catch (error) {
-        console.log(error);
+        console.log(error)
+      } finally {
+        setIsLoading(false)
       }
     }
 
-    Fetch();
-  },[id])
-   
+    Fetch()
+  }, [id])
+
+  if (isLoading) return <div>Loading</div>
+
   return (
-    <div style={{
-      border:"1px solid red",
-      width:"50%",
-      height:"100%",
-      display:"flex",
-      flexDirection:"column",
-      justifyContent:"center",
-      alignItems:"center",
-      margin:"auto",
-      textAlign:"center"
-    }}>
-      <img src={single.thumbnail} alt="" />
-      <h1>{single.title}</h1>
-      <h4>{Math.floor(single.price)}</h4>
+    <div className="single-product">
+      <img src={thumbnail} alt="" />
+      <h1>{title}</h1>
+      <h4>{Math.floor(price) || 0}</h4>
     </div>
   )
 }
